7d335e77d399f67e085916d81523e9e88f794bbe
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-12-14 18:55:55 -0300

    Obtendo um usuário por id

--- src/main/java/com/matheusvieiras/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusvieiras/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.matheusvieiras.workshopmongo.services;
 
 import com.matheusvieiras.workshopmongo.domain.User;
 import com.matheusvieiras.workshopmongo.repository.UserRepository;
+import com.matheusvieiras.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/matheusvieiras/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.matheusvieiras.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
