72e022beb002b50aaa61e95eea067019e9b26867
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-12-15 16:08:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/matheusvieiras/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.matheusvieiras.workshopmongo.repository;
+
+import com.matheusvieiras.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
