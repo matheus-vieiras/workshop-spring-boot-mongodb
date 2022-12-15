74189663539414d36c35a3f48c0736b7973b1ffc
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-12-15 15:02:32 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/matheusvieiras/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheusvieiras/workshopmongo/resources/UserResource.java
@@ -38,8 +38,14 @@ public class UserResource {
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
         User obj = service.fromDTO(objDto);
         obj = service.insert(obj);
-        //pegar o endereco do novo obj que inseri
+        //pegar o endereco do novo obj que inseri no header
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
