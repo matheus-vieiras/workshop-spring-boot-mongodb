a70a48a8bb95571a77f71ebc4d23231bbd641502
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-12-16 14:55:35 -0300

    Consulta simples com query methods

--- src/main/java/com/matheusvieiras/workshopmongo/services/PostService.java
+++ src/main/java/com/matheusvieiras/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.matheusvieiras.workshopmongo.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
